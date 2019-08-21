f5a1e2eb6a1d79e844da9573b955f9af33aa6d80
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2019-08-21 11:07:09 +0200

    update entities, managers, mysql conf, and dependency class with PDO php class at mysqli class place.

--- classes/datas/managers/User.php
+++ classes/datas/managers/User.php
@@ -9,9 +9,11 @@ use mvc_router\data\gesture\Manager;
 /**
  * Class User
  *
- * @method \mvc_router\data\gesture\pizzygo\entities\User get_all_from_id(int $id)
- * @method void  set_firstname_lastname_address_email(string $firstname, string $lastname, string $address, string $email)
- * @method void  del_where_id_and_pseudo_or()
+ * @method \mvc_router\data\gesture\pizzygo\entities\User|null get_all_from_id(int $id)
+ * @method \mvc_router\data\gesture\pizzygo\entities\User[]|\mvc_router\data\gesture\pizzygo\entities\User get_all_from_id_lastname(int $id, string $lastname)
+ * @method bool  set_firstname_lastname_address_email(string $firstname, string $lastname, string $address, string $email)
+ * @method bool  del_where_id()
+ * @method bool	 update_id_firstname_lastname_address(int $id, string $first_name, string $last_name, string $address)
  *
  * @package mvc_router\data\gesture\pizzygo\managers
  */
